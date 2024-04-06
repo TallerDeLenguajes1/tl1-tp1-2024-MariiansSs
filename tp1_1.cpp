63a9645ee3a979a05d183f5d8c4dd370edb53a06
Mariano <marianogotardo@gmail.com>
2024-04-06 18:02:28 -0300

    punto 3)f)

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,18 @@
 int main()
 {
     printf("Hola Mundo");
+    int *pEntero, num;
+
+    pEntero = &num;
+    printf("Ingrese un numero entero");
+    scanf("%d", &num);
+
+    printf("El contenido del puntero es: %d\n", *pEntero);
+    printf("La direccion de memoria almacenada en el puntero es: %p\n", pEntero);
+    printf("La direccion de memoria de la variable es: %p\n", &num);
+    printf("La direccion de memoria del puntero: %p\n", &pEntero);
+    printf("El tamaño de memoria utilizado por la variable: %d bytes\n", sizeof(num));
+
+
     return 0;
 }
